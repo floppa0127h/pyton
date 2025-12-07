4fcb4ea174a832e478c160b69ff59bdf096ec8a3
Floppa0127h <floppa0127h@gmail.com>
2025-12-07 16:18:06 +0200

    First commit

---
+++ L4.py
@@ -0,0 +1,36 @@
+# # L4
+# # calculator de note
+# # scrie un progam care primesncte de la tastatura o nota 
+# # daca nota este 10 sau 9 afiseasa "forate bine"
+# # daca nota este 8 sau 7 afiseasza "bine"
+# # daca este 6 sau 5 afisesasa "sufisients
+# #daca este sub 5 afiseasa "infufisient"
+# nota=int(input("care este nota elevuliu? "))
+# if nota==10 or nota== 9:
+#     print("foarte bine!")
+# if nota==8 or nota== 7:
+#     print("bine")
+# if nota== 6 or nota== 5: 
+#     print("suficient")
+# if nota<5 and nota>0:
+#     print("insuficient")
+# else:
+#     print ("error")
+# e2 
+# scirre un progam care primeste un numar
+# si care verificate
+# daca este par si numarul este mai mare decat 20 afiseasza "numar mare par"
+# daca este impar afiseasza "numar impar"
+# # daca este par si numarul este mai mic de 20 afiseasa "numar mic"
+# number=int(input("introduce a number: "))
+# if number>0:
+#     if number>20 and number%2==0:
+#         print ("big,even")
+#     if number>20 and number%2==1:
+#         print ("big,odd")
+#     if number<20 and number%2==0:
+#         print ("small even")
+#     if number<20 and number%2==1:
+#         print ("small odd")
+# else:
+#     print ("error:introduce a positiv number")
